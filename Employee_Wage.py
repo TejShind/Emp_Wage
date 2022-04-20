ceabb9399001a39301c67a2a20c9b5444c922464
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-20 12:03:35 +0530

    [Tejaswini]Add:Python program checking Employee Present or Absent

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,1 +1,19 @@
+'''
+@Author: Tejaswini Shinde
+@Date: 2022-04-20 09:31
+@Last Modified by: Tejaswini Shinde
+@Last Modified time: None
+@Title :Employee Wage
+'''
+
 print('Welcome to Employee Wage Program')
+
+#Importing Random
+import random
+IS_PRESENT = 1
+
+emp_check = random.randint(0,1)
+if emp_check == IS_PRESENT:
+    print("Employee is present")
+else:
+    print("Employee is absent")
