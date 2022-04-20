ffd63aa35aba8c1684151fe8228d5b06632b4125
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-20 20:30:44 +0530

    [Tejaswini]Add:Calculate Daily employee wage for Part time

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,9 +1,9 @@
 '''
 @Author: Tejaswini Shinde
-@Date: 2022-04-20 14:04
+@Date: 2022-04-20 20:18
 @Last Modified by: Tejaswini Shinde
 @Last Modified time: None
-@Title :UC1_Check Employee Present or absent
+@Title :UC3_Add Part time 
 '''
 
 print('Welcome to Employee Wage Program')
@@ -12,8 +12,11 @@ print('Welcome to Employee Wage Program')
 import random
 
 #CONSTANTS
-IS_PRESENT = 1
+IS_ABSENT = 0
+IS_PART_TIME = 1
+IS_PRESENT = 2
 WAGE_PER_HOUR = 20
+PART_TIME_HOUR = 4
 FULL_DAY_HOUR = 8
 
 
@@ -23,9 +26,11 @@ def get_work_hours():
             Parameter: None
             Return: work_hrs as per Random Calculation
         '''
-    emp_check = random.randint(0,1)
+    emp_check = random.randint(0,2)
     if emp_check == IS_PRESENT:
         work_hrs = FULL_DAY_HOUR
+    elif emp_check == IS_PART_TIME:
+        work_hrs = PART_TIME_HOUR
     else:
         work_hrs = 0
     return work_hrs
