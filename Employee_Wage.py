f79912e665d25d152487f4031f52df16771d51ff
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-20 19:32:14 +0530

    [Tejaswini]Add:Calculate Daily employee wage

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,19 +1,34 @@
 '''
 @Author: Tejaswini Shinde
-@Date: 2022-04-20 09:31
+@Date: 2022-04-20 14:04
 @Last Modified by: Tejaswini Shinde
 @Last Modified time: None
-@Title :Employee Wage
+@Title :UC1_Check Employee Present or absent
 '''
 
 print('Welcome to Employee Wage Program')
 
 #Importing Random
 import random
-IS_PRESENT = 1
 
-emp_check = random.randint(0,1)
-if emp_check == IS_PRESENT:
-    print("Employee is present")
-else:
-    print("Employee is absent")
+#CONSTANTS
+IS_PRESENT = 1
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
+
+def get_work_hours():
+    '''
+            Description: Function to get Employee Working hours
+            Parameter: None
+            Return: work_hrs as per Random Calculation
+        '''
+    emp_check = random.randint(0,1)
+    if emp_check == IS_PRESENT:
+        work_hrs = FULL_DAY_HOUR
+    else:
+        work_hrs = 0
+    return work_hrs
+
+daily_emp_wage = WAGE_PER_HOUR * get_work_hours()
+print(f"Person earns {daily_emp_wage} rupees this day")
