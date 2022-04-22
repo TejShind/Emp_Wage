70cf798aa1ad09bc8ea19b48a5262fa4f4938e20
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-22 08:42:53 +0530

    [Tejaswini]Add:Refactor code and added Daily employee wage for Part time

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,39 +1,37 @@
 '''
 @Author: Tejaswini Shinde
-@Date: 2022-04-20 20:18
+@Date: 2022-04-21 20:43
 @Last Modified by: Tejaswini Shinde
 @Last Modified time: None
-@Title :UC3_Add Part time 
+@Title :Employee Wage UC3_Part Time
 '''
 
 print('Welcome to Employee Wage Program')
-
-#Importing Random
 import random
 
-#CONSTANTS
-IS_ABSENT = 0
-IS_PART_TIME = 1
-IS_PRESENT = 2
-WAGE_PER_HOUR = 20
-PART_TIME_HOUR = 4
-FULL_DAY_HOUR = 8
+class Employee:
+    def __init__(self):
+        self.attendance = random.randint(0, 1)
+    """
 
+    Below functions checks whether the employee is present or absent. And print the status
+    Calculate Daily Wage assume Wage per hour=20,Fullday hour=8,Part time day Hour=4
+    """
 
-def get_work_hours():
-    '''
-            Description: Function to get Employee Working hours
-            Parameter: None
-            Return: work_hrs as per Random Calculation
-        '''
-    emp_check = random.randint(0,2)
-    if emp_check == IS_PRESENT:
-        work_hrs = FULL_DAY_HOUR
-    elif emp_check == IS_PART_TIME:
-        work_hrs = PART_TIME_HOUR
-    else:
-        work_hrs = 0
-    return work_hrs
+    def check_attendance(self):
+        if self.attendance == 0:
+            print("Employee is Absent")
+        elif self.attendance ==1:
+            self.attendance = random.randint(0, 1)  
+            print("Employee is Present ")   
+            daily_wage=20*8
+            print(f"The daily employee wage is Rs.{daily_wage}")
+        else:
+            print("Employee is part time prsent")
+            daily_wage=20*4
+            print(f"The daily employee wage is Rs.{daily_wage}")
+            employee = Employee()
+            employee.check_attendance()
+            employee.daily_emp_wage()
+            
 
-daily_emp_wage = WAGE_PER_HOUR * get_work_hours()
-print(f"Person earns {daily_emp_wage} rupees this day")
