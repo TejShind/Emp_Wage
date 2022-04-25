a6b4fe593ddfd6ceb3170476b94c32115943856c
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-25 17:11:13 +0530

    [Tejaswini]Add: Solving using Case statment

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -3,35 +3,88 @@
 @Date: 2022-04-21 20:43
 @Last Modified by: Tejaswini Shinde
 @Last Modified time: None
-@Title :Employee Wage UC3_Part Time
+@Title :Employee Wage UC4_Solving using Case Statment
 '''
 
-print('Welcome to Employee Wage Program')
 import random
 
-class Employee:
-    def __init__(self):
-        self.attendance = random.randint(0, 1)
-    """
+print('Welcome to Employee Wage Program')
 
-    Below functions checks whether the employee is present or absent. And print the status
-    Calculate Daily Wage assume Wage per hour=20,Fullday hour=8,Part time day Hour=4
-    """
 
-    def check_attendance(self):
-        if self.attendance == 0:
-            print("Employee is Absent")
-        elif self.attendance ==1:
-            self.attendance = random.randint(0, 1)  
-            print("Employee is Present ")   
-            daily_wage=20*8
-            print(f"The daily employee wage is Rs.{daily_wage}")
-        else:
-            print("Employee is part time prsent")
-            daily_wage=20*4
-            print(f"The daily employee wage is Rs.{daily_wage}")
-            employee = Employee()
-            employee.check_attendance()
-            employee.daily_emp_wage()
-            
+fullDayEmpHour = 8
+partTimeEmpHour = 4
+empWagePerHour = 20
+
+
+
+def empFullDayWage():
+    """
+        Description:
+            checking employee full day wage
+        Parameter:
+            none
+        Return:
+            sending total emp wage and message of full day
+    """
+    totalEmpWage = fullDayEmpHour * empWagePerHour
+
+    return("Employee Is Present And Done Full Day", totalEmpWage)
+
+
+
+def empPartTimeDayWage():
+    """
+        Description:
+            checking employee half day wage
+        Parameter:
+            none right now
+        Return:
+            sending total emp wage and message of half day
+    """
+    totalEmpWage = partTimeEmpHour * empWagePerHour
+
+    return("Employee Is Present And Done half Day", totalEmpWage)
+
+
+def empPresentyCheck(presentCheck):
+    """
+        Description:
+            checking employee attendace present or absent
+        Parameter:
+            sending parameter of two random number
+        Return:
+            return absent message
+    """
+    switcher = {
+        0: 'Employee Is Absent',
+        1: empwageCalculate(random.randint(0, 1)),
+    }
+    return switcher[presentCheck]
+
+def empwageCalculate(num):
+    """
+        Description:
+            checking employee half day or full day
+        Parameter:
+            sending parameter of two random number for checking full day or half day
+        Return:
+            return function values from full or half day
+    """
+    switcher = {
+        0: empPartTimeDayWage(),
+        1: empFullDayWage()
+    }
+    return switcher[num]
+
+
+empCheck = random.randint(0, 1)
+
+employeeWage = empPresentyCheck(empCheck)
+
+if empCheck == 0:
+    print(employeeWage)
+else:
+    print(employeeWage[0])
+    print(employeeWage[1])
+
 
